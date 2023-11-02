9d7d2d477d0d888d0c5f0bd6bd9628bcf8409159
vivek penuli <63710710+vivekpenuli@users.noreply.github.com>
2023-11-02 09:09:19 +0530

    Update Brute_Force_String_Matching.c

--- Brute_Force_String_Matching.c
+++ Brute_Force_String_Matching.c
@@ -1,1 +1,35 @@
+package algo;
+
+public class BruteForceStringMatch {
+    public static void main(String[] args) {
+        String string = "ABCABAB ABABABAABAC";
+        String pattern = "ABABAABA";
+
+        stringSearch(string, pattern);
+    }
+
+    // Brute-force string search method
+    private static void stringSearch(String string, String pattern) {
+        int sLen = string.length();
+        int pLen = pattern.length();
+        boolean found = false;
+        int i;
+        for(i = 0; i < sLen - pLen + 1; i++) {
+            int j = 0;
+            for (; j < pLen; j++) {
+                if (string.charAt(i + j) != pattern.charAt(j))
+                    break;
+            }
+            if (j == pLen) {       // If we have reached end of pattern, we have found the pattern in string
+                found = true;
+                break;
+            }
+        }
+        if (found) {
+            System.out.println("Found pattern at index: " + i);
+        } else {
+            System.out.println("Could not find pattern");
+        }
+    }
+}
 
