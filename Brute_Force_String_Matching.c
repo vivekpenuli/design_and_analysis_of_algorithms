143bb95c5a52b98fbda0c971694404cf5de34c39
vivek penuli <63710710+vivekpenuli@users.noreply.github.com>
2023-11-02 09:09:46 +0530

    Update Brute_Force_String_Matching.c

--- Brute_Force_String_Matching.c
+++ Brute_Force_String_Matching.c
@@ -1,35 +1,35 @@
-package algo;
+#include <stdio.h>
+#include <string.h>
 
-public class BruteForceStringMatch {
-    public static void main(String[] args) {
-        String string = "ABCABAB ABABABAABAC";
-        String pattern = "ABABAABA";
-
-        stringSearch(string, pattern);
-    }
-
-    // Brute-force string search method
-    private static void stringSearch(String string, String pattern) {
-        int sLen = string.length();
-        int pLen = pattern.length();
-        boolean found = false;
-        int i;
-        for(i = 0; i < sLen - pLen + 1; i++) {
-            int j = 0;
-            for (; j < pLen; j++) {
-                if (string.charAt(i + j) != pattern.charAt(j))
-                    break;
-            }
-            if (j == pLen) {       // If we have reached end of pattern, we have found the pattern in string
-                found = true;
+// Brute-force string search function
+void string_search(char *string, char *pattern, int s_len, int p_len) {
+    int i = 0;
+    int found = 0;
+    for(; i < s_len - p_len + 1; i++) {
+        int j = 0;
+        for (; j < p_len; j++) {
+            if (string[i + j] != pattern[j])
                 break;
-            }
         }
-        if (found) {
-            System.out.println("Found pattern at index: " + i);
-        } else {
-            System.out.println("Could not find pattern");
+        if (j == p_len) {   // If we have reached end of pattern, we have found the pattern in string
+            found = 1;
+            break;
         }
     }
+    if (found)
+        printf("Found pattern at index: %d\n", i);
+    else
+        printf("Could not find the pattern\n");
 }
 
+// Driver function
+int main() {
+    char *string = "ABCABAB ABABABAABAC";
+    char *pattern = "ABABAABA";
+    int s_len = strlen(string);
+    int p_len = strlen(pattern);
+
+    string_search(string, pattern, s_len, p_len);
+
+    return 0;
+}
