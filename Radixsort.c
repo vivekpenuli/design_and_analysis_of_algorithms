96709ab1499e7404a35d642fa430da60e32f4820
vivek penuli <63710710+vivekpenuli@users.noreply.github.com>
2023-11-21 10:20:45 +0530

    Update Radixsort.c

--- Radixsort.c
+++ Radixsort.c
@@ -1,1 +1,67 @@
+#include <stdio.h>
+
+int getMax(int arr[], int n) {
+    int max = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+
+void countingSort(int arr[], int n, int exp) {
+    const int base = 10; 
+
+    int output[n]; // Output array
+    int count[base];
+
+    for (int i = 0; i < base; i++) {
+        count[i] = 0;
+    }
+
+    for (int i = 0; i < n; i++) {
+        count[(arr[i] / exp) % base]++;
+    }
+
+    // Change count[i] so that count[i] contains the actual
+    // position of this digit in the output[]
+    for (int i = 1; i < base; i++) {
+        count[i] += count[i - 1];
+    }
+
+    for (int i = n - 1; i >= 0; i--) {
+        output[count[(arr[i] / exp) % base] - 1] = arr[i];
+        count[(arr[i] / exp) % base]--;
+    }
+
+    for (int i = 0; i < n; i++) {
+        arr[i] = output[i];
+    }
+}
+
+void radixSort(int arr[], int n) {
+    int max = getMax(arr, n);
+
+    for (int exp = 1; max / exp > 0; exp *= 10) {
+        countingSort(arr, n, exp);
+    }
+}
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {170, 45, 75, 90, 802, 24, 2, 66};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    radixSort(arr, n);
+    printf("Sorted array: ");
+    printArray(arr, n);
+    return 0;
+}
+
 
